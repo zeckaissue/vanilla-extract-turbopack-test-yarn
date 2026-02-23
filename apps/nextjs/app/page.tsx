336afebea2fd896b5da8879bfb1bea559ec7d8bd
ZecKa <18116930+zecka@users.noreply.github.com>
2026-02-24 00:19:18 +0100

    force to RJWadley PR

--- apps/nextjs/app/page.tsx
+++ apps/nextjs/app/page.tsx
@@ -1,10 +1,8 @@
 import { PageCss } from "./page.css";
-import { Button } from "@acme/ui/src/button/Button";
 export default function Home() {
   return (
   <div className={PageCss}>
     Hello world
-    <Button label="Click me" />
   </div>
   );
 }
